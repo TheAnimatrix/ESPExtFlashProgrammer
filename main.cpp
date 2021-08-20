f0e397e368bb4e8ea42bab196d72b47008078bfd
TheAnimatrix <avrsaditya@gmail.com>
2021-08-20 23:58:00 +0530

    Genesis

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <Arduino.h>
+#include <SPIMemory.h>
+
+SPIFlash flash(D1); //setup spi flash w/ cs set to D1(GPIO5)
+void setup()
+{
+  flash.begin(); //init
+  Serial.begin(9600);
+}
+
+enum State
+{
+  READY,
+  WRITE,
+  READ,
+  ERASE
+};
+State state = READY;
+char rbuf[37];
+char ibuf[32];
+
+//spi flash offsets.
+int wr_offset = 0x0;
+int rd_offset = 0x0;
+uint8_t bcount = 0x0;
+
+//COMMANDS: (4 letter)
+//IGNR -> IGNORE & CONTINUE
+//WRIT -> WRITE
+//STOP -> STOP EVERYTHING && RETURN TO READY
+//READ -> READ
+
+//DATA SENT FROM PC SHOULD BE [CMD(4) + DATA(32)]
+
+union packed
+{
+  uint32_t int32;
+  byte bytes[4];
+};
+
+void clearBufs()
+{
+  for (int i = 0; i < 37; i++)
+    rbuf[i] = 0;
+  for (int i = 0; i < 32; i++)
+    ibuf[i] = 0;
+}
+
+void loop()
+{
+  switch (state)
+  {
+  case READY:
+    if (Serial.available() > 0)
+    {
+      Serial.readBytes(rbuf, 4);
+      if (strncmp(rbuf, "WRIT", 4) == 0)
+        state = WRITE;
+      else if (strncmp(rbuf, "RDID", 4) == 0)
+      {
+        packed JEDEC;
+        JEDEC.int32 = flash.getJEDECID();
+        Serial.write(JEDEC.bytes, 4);
+      }
+      else if (strncmp(rbuf, "READ", 4) == 0)
+      {
+        byte cbuf[4];
+        Serial.readBytes(cbuf, 4);
+        uint32_t addr;
+        addr = (int)*(cbuf) << 24; //send from pc using big endian
+        addr += (int)*(cbuf + 1) << 16;
+        addr += (int)*(cbuf + 2) << 8;
+        addr += (int)*(cbuf + 3);
+        char rrbuf[32];
+        bool result = flash.readCharArray(addr, rrbuf, 32);
+        if (result)
+        {
+          Serial.print("PASS");
+          Serial.write(rrbuf, 32);
+        }
+        else
+          Serial.print("FAIL");
+      }
+      else if (strncmp(rbuf, "ERAS", 4) == 0)
+      {
+        bool result = flash.eraseChip();
+        clearBufs();
+        if (result)
+          Serial.print("PASS");
+        else
+          Serial.print("FAIL");
+      }
+      else if (strncmp(rbuf, "ECHO", 4) == 0)
+        Serial.print("REDY");
+      else
+        state = READY;
+    }
+    else
+    {
+      state = READY;
+    }
+    break;
+  case WRITE:
+    if (Serial.available() > 3)
+    {
+      Serial.readBytes(rbuf, 37); //CMD(4) + COUNT(1) + DATA(32)
+      bcount = (int)*(rbuf + 4);
+      if (strncmp(rbuf, "IGNR", 4) == 0)
+      {
+        //open spi if closed
+        //write *data* to spi and confirm somehow
+        bcount = (int)*(rbuf + 4);
+        //write *ibuf*(data) to spi
+        bool result = flash.writeCharArray(wr_offset, rbuf + 5, bcount);
+        wr_offset = wr_offset+bcount;
+        //done
+        if (result)
+          Serial.print("PASS");
+        else
+          Serial.print("FAIL");
+      }
+      else if (strncmp(rbuf, "WRIT", 4) == 0) //WRIT while already in WRITE indicates a start pos change specified in hex(data)
+      {
+        state = WRITE;
+        wr_offset = (int)*(rbuf + 5) << 24; //send from pc using big endian
+        wr_offset += (int)*(rbuf + 6) << 16;
+        wr_offset += (int)*(rbuf + 7) << 8;
+        wr_offset += (int)*(rbuf + 8);
+        Serial.print("OKAY");
+        packed l;
+        l.int32 = wr_offset;
+        Serial.write(l.bytes, 4);
+      }
+      else if (strncmp(rbuf, "STOP", 4) == 0) //RETURN TO READY
+      {
+        state = READY;
+        Serial.print("OKAY");
+      }
+    }
+    break;
+  default:
+    break;
+  }
+}
---
+++ write_flash.py
@@ -0,0 +1,181 @@
+import serial.tools.list_ports
+from serial import Serial
+import sys
+import time
+import os.path
+import struct
+ports = serial.tools.list_ports.comports()
+
+def selectCOMPort(custom=None):
+    if(custom!=None and len(custom)>0):
+        val = custom
+    else:
+        val = input("\nEnter COM Port of MCU:")
+    if any(x[0] == val for x in ports):
+        return val
+    else:
+        return -1
+
+def checkAvailability(no_print=False):
+    ser.flushInput()
+    ser.flushOutput()
+    ser.write(b'ECHO')
+    read_val = ser.read(size=4)
+    if(not no_print):
+        print(read_val)
+    w = 'n'
+    if(read_val == b"REDY"):
+        if(not no_print):
+            print("Device Ready!")
+        return 1
+    else:
+        if(not no_print):
+            w = input("Device not responding, try again? (y/n)")
+    if w == 'y':
+        ser.write(b'STOP')
+        ser.read(4)
+        return checkAvailability()
+    else:
+        return -1
+
+def sendPrep(offset):
+    ser.write(b'WRIT')
+    ser.write(b'WRIT'+bin(offset))
+
+def sendData(bdata32,len,timeout):
+    if(len > 32):
+        len = 32
+    ser.write(b'IGNR'+bin(len)+bdata32)
+    response = ser.read(size=4)
+    print(response)
+
+def readID(timeout):
+    ser.write(b'STOP')
+    tryf = timeout
+    while(tryf != 0):
+        if(checkAvailability(no_print=True)):
+            ser.write(b'RDID');
+            jedec_id = ser.read(4);
+            jedec_id = hex(struct.unpack('<i',jedec_id)[0])
+            return jedec_id
+        else:
+            tryf = tryf-1;
+            time.sleep(1);
+            continue
+    return -1;
+
+
+#main code executes from here
+
+force = False
+file = ""
+in_port = ""
+offset = 0x0;
+
+for argv in sys.argv:
+    if argv == '-f':
+        force = True
+    if "--file=" in argv:
+        file = argv.split("=")[1]
+        if(len(file)>0 and os.path.isfile(file)):
+            print("file validated.")
+        else:
+            print("Invalid File Path\nPlease specify proper file to flash using --file=<file_path>")
+            exit()
+    if "--port=" in argv:
+        in_port = argv.split("=")[1]
+        if(len(in_port)<=0):
+            print("Invalid Port, please specify as follows : --port=<port_name>")
+            exit()
+    if "--offset=" in argv:
+        offset = argv.split("=")[1]
+        if(len(offset)<=0):
+            print("Invalid Offset, please specify proper offset as follows : --offset=<0x300000>")
+            exit()
+        try:
+            offset = int(offset,16)
+        except:
+            print("Invali Offset Format! Please specify in HEX as follows : --offset=<0x300000>")
+
+if (len(file) == 0 ):
+    print("Please specify file to flash using --file=<file_path>")
+    exit()
+
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+
+try_again = True;
+g_port = ""
+while try_again:
+    if(len(in_port)==0):
+        result = selectCOMPort()
+    else:
+        result = selectCOMPort(in_port)
+    if(result == -1):
+        x = input("Invalid port, try again (y/n)?")
+        if x == 'y' or x == 'Y':
+            try_again = True
+            in_port=""
+        else:
+            try_again = False
+            exit()
+    else:
+        try_again = False
+        g_port = result
+        print(f"Port {g_port} selected")
+
+baud_rate = 9600
+ser = Serial(g_port,baud_rate,timeout=5)
+rfile = open(file,'r+b')
+checkAvailability()
+print("\ndevice details:")
+print("jedec id    : " +hex(int(readID(5),16)>>16))
+print("memory type : " +hex(int(readID(5),16)>>8 & 0xFF))
+print("capacity    : " +hex(int(readID(5),16) & 0xFF))
+if(not force):
+    w = input("\nProceed(y/n) ?")
+    if(not(w == 'y' or w == 'Y')):
+        print("exiting...")
+        exit()
+
+print("Erasing chip... (This might take 15-30s)")
+ser.write(b'ERAS')
+ser.timeout = 60
+res = ser.read(4)
+ser.timeout = 5
+if(res != b'PASS'):
+    print(f"Erase chip operation failed! w/ response : {res}")
+    ser.close()
+    exit()
+ser.write(b'WRIT')
+ser.write(b'WRIT'+int.to_bytes(4,1,'big')+int.to_bytes(offset,4,'big'));
+if(ser.read(4) == b'OKAY'):
+    off = ser.read(4)
+    off = hex(struct.unpack('<i',off)[0])
+    print(f"offset {off} successfully written")
+else:
+    print("offset write error!")
+    exit()
+
+lena=1
+tlen=0
+print("Writing chip...")
+ser.timeout = 15
+while lena > 0:
+    a = rfile.read(32)
+    lena = len(a)
+    if(lena == 0):
+        break
+    print(f"{tlen} bytes written, last {lena} bytes: {a}", end='\r')
+    ser.write(b'IGNR'+int.to_bytes((lena&0xFF),1,'big')+a)
+    result = ser.read(4)
+    if(result!=b'PASS'):
+        print(f"write failed after {tlen} bytes! Exiting... RESPONSE : {result}")
+        ser.close()
+        exit()
+    tlen = tlen+lena
+
+ser.write(b'STOP')
+print("\nWrite completed")
+ser.close()
+# file.write(binascii.unhexlify(hex(num).rstrip('L').lstrip('0x')))
